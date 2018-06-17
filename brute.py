c1dba092b68c0d04728993203744afb56325430d
daniel <b04902002@ntu.edu.tw>
2018-06-17 17:23:36 +0800

    add argparser

--- brute.py
+++ brute.py
@@ -3,6 +3,18 @@ import random
 import string
 import time
 from bs4 import BeautifulSoup as bs
+import argparse
+import urllib.parse
+
+parser = argparse.ArgumentParser(description='Usage example: python brute.py --base bit.ly --num 100 --len 6')
+parser.add_argument('--base', dest='base',
+                    default="goo.gl/", help='base url')
+parser.add_argument('--num', dest='num', type=int,
+                    default=500, help='the number of url')
+parser.add_argument('--len', dest='len', type=int,
+                    default=5, help='the length of token')
+args = parser.parse_args()
+
 
 words = []
 counts = dict()
@@ -23,15 +35,15 @@ def random_string(length):
 
 def gen_random_url(base_url, url_num=500, token_len=5):
     for i in range(url_num):
-        url = base_url + random_string(token_len)
+        url = urllib.parse.urljoin(base_url, random_string(token_len))
         yield url
 
-def gen_chinese_url(base_url="http://bit.ly/", url_num=500):
+def gen_chinese_url(base_url="bit.ly/", url_num=500):
     word_list = words[:url_num]
     print("Minimum counts:", counts[words[url_num-1]])
 
     for word in word_list:
-        url = base_url + word
+        url = urllib.parse.urljoin(base_url, word)
         yield url
 
 
@@ -41,8 +53,10 @@ if __name__ == '__main__':
 
     url_num = 500
     valid_url = 0
+    print(args.base, args.num, args.len)
 
-    for i, url in enumerate(gen_random_url(base_url="http://goo.gl/", url_num=url_num, token_len=6)):
+    args.base = "http://" + args.base
+    for i, url in enumerate(gen_random_url(base_url=args.base, url_num=args.num, token_len=args.len)):
         origin_url = ""
         r = requests.get(url, allow_redirects=False)
         while r.status_code == 403: # 403 may represent YOU ARE BANNED!!
